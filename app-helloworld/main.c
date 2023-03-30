25b1adf95e925e3be4c04b3d68a43376970a98eb
Oleksii Moisieiev <oleksii_moisieiev@epam.com>
2023-03-30 13:39:28 +0300

    Add test for 2 sec
    Signed-off-by: Oleksii Moisieiev <oleksii_moisieiev@epam.com>

--- app-helloworld/main.c
+++ app-helloworld/main.c
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
 		printf(" %s \n",          monkey3[i++]);
 		printf(" %s ",            monkey3[i++]);
 		fflush(stdout);
-		millisleep(250);
+		millisleep(2000);
 	}
 #endif /* CONFIG_APPHELLOWORLD_SPINNER */
 
